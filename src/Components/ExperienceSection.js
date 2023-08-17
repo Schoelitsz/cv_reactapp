f21eeb1c9cc9af2272d49b2b266110506114b6b4
Terrinia <86646827+Schoelitsz@users.noreply.github.com>
2023-08-17 12:00:08 +0200

    filling the classes

---
+++ src/Components/ExperienceSection.js
@@ -0,0 +1,19 @@
+function ExperienceSection() {
+    return (
+        <div className='info-section card'>
+             <div className='card-body'>
+                 <ul>
+                    <li>
+                        Etesian
+                    </li>
+                    <li>
+                        Mediamarkt
+                    </li>
+                    <li>
+                        Teleperformance
+                    </li>
+                 </ul>
+             </div>
+         </div> 
+     );
+}
---
+++ src/Components/SkillSection.js
@@ -0,0 +1,42 @@
+import React from 'react';
+//import '../styles/section.scss';
+//import 'bootstrap/dist/css/bootstrap.css';
+//import {Container, Row, Col} from 'react-bootstrap';
+// import { getIconSkills } from '.';
+// import Skill from '../../server/Models/Skill';
+import { getIconSkills } from '../../../server/Controllers/SkillsController';
+
+
+function IconSkillSection({ iconSkillsArray }) {
+    return (
+        // <Container>
+        //     <Row>
+        //         <Col>
+                   <ul>
+                        {iconSkillsArray.map((skill) => (
+                            <li key={skill.idskills}>
+                                {skill.name}  - {skill.text}
+                            </li>
+                        ))}
+                   </ul>
+        //         </Col>
+        //     </Row>
+        // </Container>
+    );
+}
+
+
+
+
+
+export {IconSkillSection};
+
+
+
+// {iconSkillsArray.map((skill) => (
+//     <div key={skill.idskills} className={`${skill.name}-frame`}>
+//         <picture>
+//             <img src={skill.image} alt={`${skill.name}-image`}></img>
+//         </picture>
+//     </div>
+// ))}
