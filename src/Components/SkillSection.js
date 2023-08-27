a3b4c7bd54b057555b61bb7badb3f2de294d7ce5
Terrinia <86646827+Schoelitsz@users.noreply.github.com>
2023-08-27 15:21:01 +0200

    added rating, changed skills

--- src/Components/SkillSection.js
+++ src/Components/SkillSection.js
@@ -1,23 +1,45 @@
 import React from 'react';
-import { Container, ProgressBar } from 'react-bootstrap';
+import { Container, ProgressBar, Row, Col } from 'react-bootstrap';
 import '../styles/scss/Section.scss';
 
 const sliderSkillsArray = [
     { skillname: 'Python', score: 70, display: '7' },
     { skillname: 'CSharp', score: 50, display: '5' },
+    { skillname: 'Javascript', score: 60, display: '6'},
     { skillname: 'HTMLCSS', score: 65, display: '6.5' },
+    { skillname: 'SQL', score: 55, display: '5.5'}
 ];
 
 function SkillBars() {
     return (
         <Container className='SkillSection'>
-            
-            {sliderSkillsArray.map((skill, index) => (
+            <Row>
+                <Col>
+                <h3 className='rating'>
+                    <span>0</span>
+                    <span>1</span>
+                    <span>2</span>
+                    <span>3</span>
+                    <span>4</span>
+                    <span>5</span>
+                    <span>6</span>
+                    <span>7</span>
+                    <span>8</span>
+                    <span>9</span>
+                    <span>10</span>
+                </h3>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                {sliderSkillsArray.map((skill, index) => (
                 <div className='progress-div' key={index}>
                     <h3>{skill.skillname}</h3>
                     <ProgressBar className='bg-progress' now={skill.score} variant={skill.skillname} />
                 </div>
             ))}
+                </Col>
+            </Row>
            
         </Container>
     );
