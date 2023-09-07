2b0f9cbe1f80dd2222d787bc6866ed4344a5802e
Terrinia <86646827+Schoelitsz@users.noreply.github.com>
2023-09-07 11:20:02 +0200

    started career section concept

---
+++ src/Components/CareerSection.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import '../styles/scss/Section.scss';
+
+function CareerSection() {
+    return (
+        <Container>
+
+        </Container>
+
+    );
+}
+
+export default CareerSection;
--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useState, useEffect } from "react";
+//import { useState, useEffect } from "react";
 import NameSection from './NameSection';
 import Timeline from './Timeline';
 import SkillSection from './SkillSection';
 import ProjectCards from './ProjectSection';
 import ContactSection from './ContactSection';
+import CareerSection from './CareerSection';
 //import { IconSkillSection } from './IconSkills';
 
 function Display() {
@@ -15,6 +16,7 @@ function Display() {
             <SkillSection />
             <ProjectCards />
             <ContactSection />
+            <CareerSection />
             
         </div> 
     );
